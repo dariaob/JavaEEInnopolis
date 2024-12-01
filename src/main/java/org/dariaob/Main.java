3422311d3bd41d5c62fd1b48c3e9aaff05cca47f
dariaob <pandazheya@gmail.com>
2024-12-01 13:55:33 +0500

    Homework1 Maven

--- src/main/java/org/dariaob/Main.java
+++ src/main/java/org/dariaob/Main.java
@@ -2,6 +2,9 @@ package org.dariaob;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Product product = new Product(1, "Творог", 139.00, 2);
+        Product product1 = new Product(2, "Молоко", 70.50, 5);
+        System.out.println(product);
+        System.out.println(product1);
     }
 }
