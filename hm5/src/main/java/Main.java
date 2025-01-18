e51ec7b9610ade6a765a20a2f950856a7fa4a9b1
dariaob <pandazheya@gmail.com>
2025-01-19 02:09:41 +0500

    hm5 added some code from task

---
+++ hm5/src/main/java/Main.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        List<Integer> intList = new ArrayList<>();
+
+        try (BufferedReader reader =
+                     new BufferedReader(new FileReader("hm5/src/main/resources/input.txt"))) {
+            String line = reader.readLine();
+            String[] data = line.replaceAll("\\[", "")
+                    .replaceAll("]", "")
+                    .split(",");
+
+
+            for (String num : data) {
+                intList.add(Integer.valueOf(num));
+            }
+
+            // Сортировка в натуральном порядке
+            intList.sort(Comparator.naturalOrder());
+            writeDataToFile(intList, data);
+
+            intList.sort(Comparator.reverseOrder());
+            writeDataToFile(intList, data);
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+
+    }
+
+    public static void writeDataToFile(List<Integer> numbers, String[] strings) {
+        try(FileWriter fileWriter = new FileWriter("hm5/src/main/resources/output.txt", true)) {
+            fileWriter.write(Arrays.toString(strings));
+            fileWriter.write("\n");
+            fileWriter.write(numbers.toString() + System.lineSeparator());
+            fileWriter.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
